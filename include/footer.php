b5804ca9733d26541f9ca49b6fea942d4cc03e2e
sontung0511 <tunghason9a6@gmail.com>
2021-12-30 10:57:57 +0700

    can not html

---
+++ include/footer.php
@@ -0,0 +1,64 @@
+
+	<!-- footer -->
+	<footer>
+		<div class="footer-top-first">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<!-- footer first section -->
+				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi</h2>
+				<p class="footer-main mb-4">
+				Thực phẩm chức năng tên tiếng Anh là Functional Food, ở các nước Tây Âu còn gọi là Alicaments 
+				(Thực phẩm - Thuốc), Nutraceutics (Dược phẩm dinh dưỡng), Food Suplement (Thực phẩm bổ sung dinh dưỡng), ở Trung Quốc được gọi là thực phẩm bổ dưỡng bảo vệ sức khỏe… ở Việt Nam được gọi là thực phẩm chức năng hay thực phẩm đặc biệt.</p>
+				<!-- //footer first section -->
+				<!-- footer second section -->
+				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Miễn phí vận chuyển</h3>
+								<p>Đơn hàng trên 100$</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Vận chuyển nhanh</h3>
+								<p>Toàn quốc</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Tin cậy</h3>
+								<p>Sự đảm bảo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		
+		<!-- //footer third section -->
+		
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© 2020 dai ly ban hang
+				<a href="http://w3layouts.com"> TDTU</a>
+			</p>
+		</div>
+	</div>
+	<!-- //copyright -->
